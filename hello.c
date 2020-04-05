a5a8e8aee538dfb4d92f66156f639472ee6f878c
adithya355 <63191582+adithya355@users.noreply.github.com>
2020-04-05 18:04:48 +0530

    created program
    this file swaps number

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdbool.h>
+main()
+{ int a,b;
+printf("enter first number", );
+scanf("%d",&a);
+printf("enter second number", );
+scanf("%d",&b);
+printf("the swapped numbers are %d and %d",b,a );
+return 0;
+}
